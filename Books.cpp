d5c25cebcc36d4c0d9a93de12f911b22c8b79bdb
Станислав Загоровский <93772274+CepkeT@users.noreply.github.com>
2024-11-26 20:22:19 +0300

    update books.cpp

--- Books.cpp
+++ Books.cpp
@@ -4,24 +4,17 @@
 
 // Helper function to convert a string to lowercase
 std::string toLower(std::string str) {
-
 	std::transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
-
 	return lowerStr;
 }
 
 std::vector<Book> Books::findBookByTitle(const std::string& title) const 
 {
 	std::vector<Book> foundBooks;
-
 	std::string lowercaseTitle = toLower(title);
-
 	for (Book book : books) {
-
 		std::string bookTitle = toLower(book->getTitle());
-
 		if (bookTitle.find(lowerTitle) != std::string::npos) {
-
 			foundBooks.push_back(book);
 		}
 	}
@@ -29,13 +22,9 @@ std::vector<Book> Books::findBookByTitle(const std::string& title) const
 }
 
 std::vector<Book> findBookByAuthor(const std::string& author) const {
-
 	std::vector<Book> foundBooks;
-
 	for (auto& book : books) {
-
 		if (book.getAuthor() == author) {
-
 			foundBooks.push_back(book);
 		}
 	}
@@ -43,13 +32,9 @@ std::vector<Book> findBookByAuthor(const std::string& author) const {
 }
 
 std::vector<Book> findBookByInfo(const std::string& info) const {
-
 	std::vector<Book> foundBooks;
-
 	for (auto& book : books) {
-
 		if (book.getInfo().find(info) != std::string::npos) {
-
 			foundBooks.push_back(book);
 		}
 	}
@@ -57,30 +42,19 @@ std::vector<Book> findBookByInfo(const std::string& info) const {
 }
 
 std::vector<Book> findBook(const std::string& searchQuery) const {
-
 	std::vector<Book> foundBooks;
 
-
 	std::string lowerSearchQuery = toLower(searchQuery);
-
 	for (auto& book : books) {
-
 		std::string lowerTitle = toLower(book.getTitle());
-
 		std::string lowerAuthor = toLower(book.getAuthor());
-
 		std::string lowerInfo = toLower(book.getInfo());
-
 		if (lowerTitle.find(lowerSearchQuery) != std::string::npos ||
-
 			lowerAuthor.find(lowerSearchQuery) != std::string::npos ||
-
 			lowerInfo.find(lowerSearchQuery) != std::string::npos) {
-
 			foundBooks.push_back(book);
 		}
 	}
-
 	return foundBooks;
 }
 
