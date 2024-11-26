90b7578ce5a33a49f182485cbb4fedbf3d2ee995
student <student@top.local>
2024-11-26 20:31:55 +0300

    New feature deleteBookByID

--- Books.cpp
+++ Books.cpp
@@ -3,27 +3,17 @@
 #include <iostream>
 
 
-void Books::addBook(Book& book) {
+void Books::addBook(Book book) {
 	books.push_back(book);
 }
 
-bool Books::deleteBookById(int id) {
-	int index = 0;
-	bool found = false;
-	for (Book& book : books) {
-		if (book.GetId() == id) {
-			std::cout << "This book has been found " << std::endl;
-			found = true;
+bool Books::deleteBookByID(int id)
+{
+	for (Book book = books.begin(); book != books.end(); ++book) {
+		if ((*book)->getID() == id) {
+			books.erase(book);
+			return true;
 		}
-		++index;
 	}
-	if (found = true) {
-		books.erase(books.begin() + index);
-		return true;
-	}
-	else {
-		std::cout << "This book has not been found " << std::endl;
-		return false;
-	}
-
+	return false;
 }
