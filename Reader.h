e30faed3f54e8273f505a597eece1d94222ef5b4
student <student@top.local>
2024-11-26 20:18:11 +0300

    Modified Reader.h

--- Reader.h
+++ Reader.h
@@ -8,16 +8,21 @@ public:
         const int age = -1,);
 
     std::string getName() const;
+    std::string getAddress() const;
+    std::string getPhoneNumber() const;
     int getAge() const;
-    int getDebt() const;
     int getReaderID() const;
 
     void setName(const std::string& name);
+    void setAddress(const std::string& name);
+    void setPhoneNumber(const std::string& phoneNumber);
     void setAge(const int age);
-    void setDebt(const float age);
 private:
     std::string name;
-    int age;
-    float debt;
+    std::string address;
+    std::string phoneNumber;
+    unsigned int age;
     const int readerID;
+    static int count;
+
 };
